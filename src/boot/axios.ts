1f931261aeb2c1aea37b334cbee1725f1becc9eb
wizwiz <abezzi@outlook.cl>
2025-12-17 04:51:35 -0300

    fix(server): add authorization token header automatically

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,5 +1,6 @@
 import { defineBoot } from '#q-app/wrappers';
 import axios, { type AxiosInstance } from 'axios';
+import { useAuthStore } from 'src/stores/auth';
 
 declare module 'vue' {
   interface ComponentCustomProperties {
@@ -31,6 +32,30 @@ export default defineBoot(({ app }) => {
   app.config.globalProperties.$api = api;
   // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
+
+  // adds authorization header automatically
+  api.interceptors.request.use((config) => {
+    const authStore = useAuthStore();
+    if (authStore.token) {
+      config.headers.Authorization = `Bearer ${authStore.token}`;
+    }
+    return config;
+  });
+
+  // global 401 handling
+  api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response?.status === 401) {
+        const authStore = useAuthStore();
+        authStore.logout();
+        // Redirect to login (using window.location to force full reload)
+        window.location.href = '/login';
+      }
+
+      throw error;
+    },
+  );
 });
 
 export { api };
