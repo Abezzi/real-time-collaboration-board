a4aaff0f2cc2b6b0c2063901558459c4e5684737
wizwiz <abezzi@outlook.cl>
2026-01-09 13:22:24 -0300

    fix(test): vitest plugin config with examples

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,27 @@
 import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.ts',
+    environment: 'happy-dom',
+    setupFiles: 'test/vitest/setup-file.ts',
+    include: [
+      // Matches vitest tests in any subfolder of 'src' or into 'test/vitest/__tests__'
+      // Matches all files with extension 'js', 'jsx', 'ts' and 'tsx'
+      'src/**/*.vitest.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+      'test/vitest/__tests__/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+    ],
   },
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      sassVariables: 'src/quasar-variables.scss',
+    }),
+    tsconfigPaths(),
+  ],
 });
